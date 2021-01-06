c2ebe588afaab8c530491029a6edb074170333d4
Tobias Hein <t.hein@brenk.com>
2021-01-06 08:34:44 +0100

    initialisation

---
+++ helper.py
@@ -0,0 +1,8 @@
+# Created by He at 06.01.2021
+
+# Feature: helper functions etc. for the scraper, evaluation process and reporting
+
+# Scenario: first scraper, then evaluation, then reporting
+
+#---------------------------------------------------------
+
